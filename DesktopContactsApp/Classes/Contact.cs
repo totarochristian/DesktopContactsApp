9fb2f28b86695a7ce685e816cef190528146ab5c
totarochristian <totaro.christian.18@gmail.com>
2023-10-20 17:20:14 +0200

    Add Id property of contact class (this will be the primary key in the sql table)

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -8,6 +8,7 @@ namespace DesktopContactsApp.Classes
 {
     public class Contact
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
