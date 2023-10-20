358cbefa1b84e5a8a053b2e33b54e0ec81364045
totarochristian <totaro.christian.18@gmail.com>
2023-10-20 17:56:29 +0200

    Update App, set private database name and folder path

--- DesktopContactsApp/App.xaml.cs
+++ DesktopContactsApp/App.xaml.cs
@@ -14,9 +14,9 @@ namespace DesktopContactsApp
     public partial class App : Application
     {
         //Set the database name
-        public static string databaseName = "Contacts.db";
+        static string databaseName = "Contacts.db";
         //Set the folder path equals to "My Documents" path
-        public static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         //Define the database path as a path combine of folder path + database name
         public static string databasePath = System.IO.Path.Combine(folderPath, databaseName);
     }
