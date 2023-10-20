6b83d2ffd193a28cd3aff9bd5074b95890327b42
totarochristian <totaro.christian.18@gmail.com>
2023-10-20 17:21:57 +0200

    Mark Id property as primary key for the contact table

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace DesktopContactsApp.Classes
 {
     public class Contact
     {
+        [PrimaryKey]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
