356a62c7b90b8db22a326e4780e204d3a1cd90f9
totarochristian <totaro.christian.18@gmail.com>
2023-10-20 17:01:14 +0200

    Update MainWindow, add new contact button in the stack panel and assign it a new contact btn click event

--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DesktopContactsApp
         {
             InitializeComponent();
         }
+
+        private void NewContactBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
