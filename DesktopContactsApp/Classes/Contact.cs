8f71aa1cf8453c6bb677406d137c7b28e2d7896a
totarochristian <totaro.christian.18@gmail.com>
2023-10-20 17:19:48 +0200

    Add Name, Email and Phone properties of the contact class

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -8,5 +8,8 @@ namespace DesktopContactsApp.Classes
 {
     public class Contact
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
